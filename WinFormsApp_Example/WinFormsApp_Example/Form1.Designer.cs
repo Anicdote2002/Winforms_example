53b0833cb95c8bee80107862de80ce19d8291a98
Anicdote2002 <aniruddh.chauhan.2002@gmail.com>
2023-09-07 14:15:00 -0400

    Made an example windows Form

--- WinFormsApp_Example/WinFormsApp_Example/Form1.Designer.cs
+++ WinFormsApp_Example/WinFormsApp_Example/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lstNames = new ListBox();
+            label1 = new Label();
+            txtName = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // lstNames
+            // 
+            lstNames.FormattingEnabled = true;
+            lstNames.ItemHeight = 20;
+            lstNames.Location = new Point(12, 48);
+            lstNames.Name = "lstNames";
+            lstNames.Size = new Size(194, 144);
+            lstNames.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Names";
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(214, 48);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(125, 27);
+            txtName.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(214, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(382, 353);
+            Controls.Add(button1);
+            Controls.Add(txtName);
+            Controls.Add(label1);
+            Controls.Add(lstNames);
+            Name = "Form1";
+            Text = "Names";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox lstNames;
+        private Label label1;
+        private TextBox txtName;
+        private Button button1;
     }
 }
--- WinFormsApp_Example/WinFormsApp_Example/Form1.cs
+++ WinFormsApp_Example/WinFormsApp_Example/Form1.cs
@@ -1,10 +1,21 @@
+using System.Xml.Linq;
+
 namespace WinFormsApp_Example
 {
     public partial class Form1 : Form
     {
+
+       
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
         }
     }
 }
